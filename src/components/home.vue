7dbafd0ef72d4d809c136dfc547256c0a7f81c39
paolamaric <pmar0809@gmail.com>
2020-12-02 13:12:45 +0100

    update

---
+++ src/components/home.vue
@@ -0,0 +1,13 @@
+
+
+<script>
+// @ is an alias to /src
+import home from '@/components/home.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    home
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-uppercase text-secondary font-weight-bold">Dobrodošli na aplikaciju <br> Fee Reminder</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="../about">O aplikaciji</a>
+                    </div>
+                </div>
+            </div>
+        </header>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import home from '@/components/home.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    home
   }
 }
 </script>
+
+
