936e32afe397ada3a9fabb314b85636fd3abc65f
pokornimarta <mpokorni@unipu.hr>
2021-01-04 19:24:43 +0100

    commit

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,7 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/Registration">Register</router-link>
+    <router-link to="/Registration">Register</router-link> |
+    <router-link to="/Login">Sign up</router-link>
   </div>
 </template>
 
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,45 +1,47 @@
 <template>
-            <div id="layoutAuthentication" class="bg-primary" _ngcontent-ghc-c106="">
-                <div id="layoutAuthentication_content" _ngcontent-ghc-c106="">
-                        <div class="container" _ngcontent-ghc-c118="">
-                            <div class="row justify-content-center" _ngcontent-ghc-c118="">
-                                <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 mt-4" _ngcontent-ghc-c118="">
-                                    <div class="card text-center h-100" _ngcontent-ghc-c118="">
-                                        <div class="card-body px-5 pt-5 d-flex flex-column" _ngcontent-ghc-c118="">
-                                            <div _ngcontent-ghc-c118="">
-                                                <div class="h3 text-primary font-weight-300" _ngcontent-ghc-c118=""> Create </div>
-                                                <i class="fas fa-4x fa-users text-primary mb-4" ></i>
-                                                <div id="registerclient">
-                                                <div class="col-lg-6 align-self-baseline">
-                                                    <a class="btn btn-primary btn-xl js-scroll-trigger"  href="../registrationclient">Register as a client</a>
-                                                </div>
-                                                </div>
-                                            </div>
-                                        </div>
+    <div id="layoutAuthentication" class="bg-primary" _ngcontent-ghc-c106="">
+        <div id="layoutAuthentication_content" _ngcontent-ghc-c106="">
+            <div class="container" _ngcontent-ghc-c118="">
+                <div class="row justify-content-center" _ngcontent-ghc-c118="">
+                    <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 mt-4" _ngcontent-ghc-c118="">
+                        <div class="card text-center h-100" _ngcontent-ghc-c118="">
+                            <div class="card-body px-5 pt-5 d-flex flex-column" _ngcontent-ghc-c118="">
+                                <div _ngcontent-ghc-c118="">
+                                    <div class="h3 text-primary font-weight-300" _ngcontent-ghc-c118=""> Create </div>
+                                    <i class="fas fa-4x fa-users text-primary mb-4" ></i>
+                                    <div>
+                                        <button type="button" class="btn btn-primary btn-xl js-scroll-trigger" data-toggle="modal" data-target="#exampleModal">
+                                            Register as a client
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
-                <div id="layoutAuthentication_content" _ngcontent-ghc-c106="">
-                        <div class="container" _ngcontent-ghc-c118="">
-                            <div class="row justify-content-center" _ngcontent-ghc-c118="">
-                                <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 mt-4" _ngcontent-ghc-c118="">
-                                    <div class="card text-center h-100" _ngcontent-ghc-c118="">
-                                        <div class="card-body px-5 pt-5 d-flex flex-column" _ngcontent-ghc-c118="">
-                                            <div _ngcontent-ghc-c118="">
-                                                <div class="h3 text-primary font-weight-300" _ngcontent-ghc-c118=""> Create </div>
-                                                <i class="fas fa-4x fa-user-tie text-primary mb-4" ></i>
-                                                <div class="col-lg-6 align-self-baseline">
-                                                    <a class="btn btn-primary btn-xl js-scroll-trigger" href="">Register as a host</a>
-                                                </div>
-                                            </div>
-                                        </div>
+            </div>
+        </div>
+        <div id="layoutAuthentication_content" _ngcontent-ghc-c106="">
+            <div class="container" _ngcontent-ghc-c118="">
+                <div class="row justify-content-center" _ngcontent-ghc-c118="">
+                    <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 mt-4" _ngcontent-ghc-c118="">
+                        <div class="card text-center h-100" _ngcontent-ghc-c118="">
+                            <div class="card-body px-5 pt-5 d-flex flex-column" _ngcontent-ghc-c118="">
+                                <div _ngcontent-ghc-c118="">
+                                    <div class="h3 text-primary font-weight-300" _ngcontent-ghc-c118=""> Create </div>
+                                    <i class="fas fa-4x fa-user-tie text-primary mb-4" ></i>
+                                    <div>
+                                        <button type="button" class="btn btn-primary btn-xl js-scroll-trigger" data-toggle="modal" data-target="#exampleModal2">
+                                            Register as a host
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
+            </div>
+        </div>
     <div id="layoutAuthentication_footer" _ngcontent-ghc-c106="">
       <div class="footer mt-auto footer-dark"  _ngcontent-ghc-c101="">
         <div class="container-fluid"  _ngcontent-ghc-c101="">
